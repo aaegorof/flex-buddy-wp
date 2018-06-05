f66302afcdfcaae4f5966bcb164ec64bedb7a6a2
Artemiy <artemiy@spentapp.com>
2018-06-05 14:11:05 +0300

    fixes to the sku variable

--- woocommerce/content-single-product.php
+++ woocommerce/content-single-product.php
@@ -13,7 +13,7 @@
  * @see 	    https://docs.woocommerce.com/document/template-structure/
  * @author 		WooThemes
  * @package 	WooCommerce/Templates
- * @version     3.0.0
+ * @version     3.4.0
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
@@ -22,21 +22,19 @@ if ( ! defined( 'ABSPATH' ) ) {
 global $product;
 
 
+remove_action('woocommerce_single_product_summary','woocommerce_template_single_title', 5);
 remove_action('woocommerce_single_product_summary','woocommerce_template_single_price');
 remove_action('woocommerce_single_product_summary','woocommerce_template_single_excerpt');
-remove_action('woocommerce_single_product_summary','woocommerce_template_single_add_to_cart');
-remove_action('woocommerce_single_product_summary','woocommerce_template_single_meta');
-remove_action('woocommerce_single_product_summary','woocommerce_template_single_sharing');
+remove_action('woocommerce_single_product_summary','woocommerce_template_single_meta', 40);
 
 remove_action('woocommerce_after_single_product_summary','woocommerce_output_related_products' , 20);
 remove_action('woocommerce_after_single_product_summary','woocommerce_output_product_data_tabs');
 
 
-/* add_action('shadow-metas','woocommerce_template_single_excerpt', 30); */
-/* add_action('shadow-metas','woocommerce_template_single_meta', 35); */
-add_action('shadow-metas','woocommerce_template_single_sharing', 40);
-/* add_action('shadow-metas','woocommerce_template_single_price', 45); */
-add_action('shadow-metas','woocommerce_template_single_add_to_cart', 50);
+/* add_action('attributes','woocommerce_template_single_excerpt', 30); */
+/* add_action('attributes','woocommerce_template_single_meta', 35); */
+
+/* add_action('attributes','woocommerce_template_single_price', 45); */
 
 
 ?>
@@ -54,11 +52,12 @@ add_action('shadow-metas','woocommerce_template_single_add_to_cart', 50);
 	 	return;
 	 }
 ?>
+
 <div class="container">
-  <div class="row">
-    <div class="col col-sm-8 margin-large-v lg-padding-big-r">
-      <div id="product-<?php the_ID(); ?>" <?php post_class(); ?>>
-      
+  <div id="product-<?php the_ID(); ?>" <?php post_class(); ?>>
+    
+    <div class="row">
+      <div class="col col-sm-8 margin-large-v">
       	<?php
       		/**
       		 * woocommerce_before_single_product_summary hook.
@@ -69,105 +68,39 @@ add_action('shadow-metas','woocommerce_template_single_add_to_cart', 50);
       		do_action( 'woocommerce_before_single_product_summary' );
       	?>
       </div>
-    </div>
       
-  	<div class="col col-sm-4 summary entry-summary margin-large-v md-max-padding-small-rl">
-  	<?php
-  		/**
-  		 * woocommerce_single_product_summary hook.
-  		 *
-  		 * @hooked woocommerce_template_single_title - 5
-  		 * @hooked woocommerce_template_single_rating - 10
-  		 * @hooked woocommerce_template_single_price - 10
-  		 * @hooked woocommerce_template_single_excerpt - 20
-  		 * @hooked woocommerce_template_single_add_to_cart - 30
-  		 * @hooked woocommerce_template_single_meta - 40
-  		 * @hooked woocommerce_template_single_sharing - 50
-  		 * @hooked WC_Structured_Data::generate_product_data() - 60
-  		 */
-  /* 		do_action( 'woocommerce_single_product_summary' ); */
-      the_title( '<h1 class="product_title entry-title">', '</h1>' );
-      ?>
-      
-      
-    <div class="product-text margin-big-v font-smaller">
-      
-      <div class="product-about">
-        <?php $prdct = $product -> get_data(); 
-          echo $prdct['description'];
-        ?>
-      </div>
-      
-      <?php do_action('shadow-metas') ?>
-      
-    </div> <!-- product-text -->
-  
-  	</div><!-- .summary -->
-  </div><!-- #product-<?php the_ID(); ?> end of row -->
-  
-  
-  <div class="shadow margin-big-v md-padding-big-rl md-max-padding-small-rl padding-medium-tb relative">
-    
-    <?php $product_prepare = get_field_object('product-prepare'); ?>
-    <?php if($product_prepare): ?>
-    <div class="product-prepare">
-      <div class="color-tree font-big font-medium"><?php echo $product_prepare['label'] ?></div>
-      <?php echo $product_prepare['value']; ?>
-    </div>
-    <?php endif; ?>
-    
-    <?php $product_method = get_field_object('product-method'); ?>
-    <?php if($product_method): ?>
-    <div class="product-method">
-      <div class="color-tree font-big font-medium"><?php echo $product_method['label'] ?></div>
-      <?php echo $product_method['value']; ?>
-    </div>
-    <?php endif; ?>
-      
-    <?php $product_tools = get_field_object('product-tools'); ?>
-    <?php if($product_tools): ?>
-    <div class="product-tools">
-      <div class="color-tree font-big font-medium"><?php echo $product_tools['label'] ?></div>
-      <p><?php echo $product_tools['value']; ?></p>
-    </div>
-    <?php endif; ?>
-  
-    <?php $product_time = get_field_object('product-time'); ?>
-    <?php if($product_time): ?>
-    <div class="product-time">
-      <div class="color-tree font-big font-medium"><?php echo $product_time['label'] ?></div>
-      <p><?php echo $product_time['value']; ?></p>
-    </div>
-    <?php endif; ?>
-  
-    <?php $product_spend = get_field_object('product-spend'); ?>
-    <?php if($product_spend): ?>
-    <div class="product-spend">
-      <div class="color-tree font-big font-medium"><?php echo $product_spend['label'] ?></div>
-      <p><?php echo $product_spend['value']; ?></p>
-    </div>
-    <?php endif; ?>
-  
-    <?php $product_storage = get_field_object('product-storage'); ?>
-    <?php if($product_storage): ?>
-    <div class="product-storage">
-      <div class="color-tree font-big font-medium"><?php echo $product_storage['label'] ?></div>
-      <p><?php echo $product_storage['value']; ?></p>
-    </div>
-    <?php endif; ?>
-  
-    <?php 
-      $pdf = get_field('pdf');
-      if($pdf) : ?>
-        <a class="pdf-file margin-medium-t font-semibold" href="<?php the_field('pdf') ?>" target="_blank"><img src="<?php echo get_template_directory_uri(); ?>/dist/images/pdf.svg" width="24px" valign="bottom"> Техническая характеристика <span class="color-tin">(открыть в новой вкладке)</span></a>
-      
-      <?php endif; ?>
-      
-  </div> <!-- close the additional info shadow -->
-  
-  <div class="gallery-full">
-    <?php get_template_part('template-parts/product-gallery') ?>
-  </div>
+    	<div class="col col-sm-4 summary entry-summary margin-large-v padding-medium-rl md-max-padding-small-rl">
+      	
+    	  <?php the_title( '<h1 class="product_title entry-title margin-no-b">', '</h1>' );?>
+        <p class="price color-ash font-small margin-medium-b text-left"><?php echo $product->get_price_html(); ?></p>
+        
+        <div class="product-text margin-big-b font-smaller">
+        
+          <div class="product-about margin-medium-b color-ash">
+            <?php $prdct = $product -> get_data(); 
+              echo $prdct['description'];
+            ?>
+          </div>
+        
+          <?php 
+      		/**
+      		 * woocommerce_single_product_summary hook.
+      		 *
+      		 * @hooked woocommerce_template_single_title - 5
+      		 * @hooked woocommerce_template_single_rating - 10
+      		 * @hooked woocommerce_template_single_price - 10
+      		 * @hooked woocommerce_template_single_excerpt - 20
+      		 * @hooked woocommerce_template_single_add_to_cart - 30
+      		 * @hooked woocommerce_template_single_meta - 40
+      		 * @hooked woocommerce_template_single_sharing - 50
+      		 * @hooked WC_Structured_Data::generate_product_data() - 60
+      		 */
+      		do_action( 'woocommerce_single_product_summary' ); ?>
+        
+        </div> <!-- product-text -->
+    	</div><!-- .summary -->
+  	
+    </div><!-- #product-<?php the_ID(); ?> end of row -->
   
   
   <?php
@@ -181,9 +114,11 @@ add_action('shadow-metas','woocommerce_template_single_add_to_cart', 50);
   	do_action( 'woocommerce_after_single_product_summary' );
   ?>
     
+  </div> <!-- End of id = product -->
+  
   
   <!-- <?php echo do_shortcode('[su_posts template="templates/faq-loop.php" posts_per_page="4" tax_term="107"]'); ?> -->
   
   <?php do_action( 'woocommerce_after_single_product' ); ?>
 
-</div>
+</div> <!-- End of container -->
--- woocommerce/single-product/add-to-cart/variable.php
+++ woocommerce/single-product/add-to-cart/variable.php
@@ -10,104 +10,113 @@
  * happen. When this occurs the version of the template file will be bumped and
  * the readme will list any important changes.
  *
- * @see 	https://docs.woocommerce.com/document/template-structure/
- * @author  WooThemes
+ * @see https://docs.woocommerce.com/document/template-structure/
  * @package WooCommerce/Templates
- * @version 3.0.0
+ * @version 3.4.0
  */
-if ( ! defined( 'ABSPATH' ) ) {
-	exit;
-}
+
+defined( 'ABSPATH' ) || exit;
 
 global $product;
 
 $attribute_keys = array_keys( $attributes );
 
-
-remove_action( 'woocommerce_single_variation','woocommerce_single_variation' );
-
-
 do_action( 'woocommerce_before_add_to_cart_form' ); ?>
 
-
-
-<form class="variations_form cart" method="post" enctype='multipart/form-data' data-product_id="<?php echo absint( $product->get_id() ); ?>" data-product_variations="<?php echo htmlspecialchars( wp_json_encode( $available_variations ) ) ?>">
+<form class="variations_form cart padding-medium-tb" action="<?php echo esc_url( apply_filters( 'woocommerce_add_to_cart_form_action', $product->get_permalink() ) ); ?>" method="post" enctype='multipart/form-data' data-product_id="<?php echo absint( $product->get_id() ); ?>" data-product_variations="<?php echo htmlspecialchars( wp_json_encode( $available_variations ) ); // WPCS: XSS ok. ?>">
 	<?php do_action( 'woocommerce_before_variations_form' ); ?>
 
 	<?php if ( empty( $available_variations ) && false !== $available_variations ) : ?>
-		<p class="stock out-of-stock"><?php _e( 'This product is currently out of stock and unavailable.', 'woocommerce' ); ?></p>
+		<p class="stock out-of-stock"><?php esc_html_e( 'This product is currently out of stock and unavailable.', 'woocommerce' ); ?></p>
 	<?php else : ?>
-		<table class="variations" cellspacing="0">
-			<tbody>
-  			
-  			<?php if ( wc_product_sku_enabled() && ( $product->get_sku() || $product->is_type( 'variable' ) ) ) : ?>
-        <tr class="product_meta">
-          <td class="label"><label><?php esc_html_e( 'SKU:', 'woocommerce' ); ?></label></td>
-          <td class="sku_wrapper color-tin">
-             <span class="sku"><?php echo ( $sku = $product->get_sku() ) ? $sku : esc_html__( 'N/A', 'woocommerce' ); ?></span>
-          </td>
-        </tr>
-        <?php endif; ?>
-        
-  			</tr>
-				<?php foreach ( $attributes as $attribute_name => $options ) : ?>
-					<tr>
-						<td class="label"><label for="<?php echo sanitize_title( $attribute_name ); ?>"><?php echo wc_attribute_label( $attribute_name ); ?>:</label></td>
-						<td class="value">
-							<?php
-								$selected = isset( $_REQUEST[ 'attribute_' . sanitize_title( $attribute_name ) ] ) ? wc_clean( stripslashes( urldecode( $_REQUEST[ 'attribute_' . sanitize_title( $attribute_name ) ] ) ) ) : $product->get_variation_default_attribute( $attribute_name );
-								wc_dropdown_variation_attribute_options( array( 'options' => $options, 'attribute' => $attribute_name, 'product' => $product, 'selected' => $selected ) );
-							?>
-						</td>
-					</tr>
-				<?php endforeach;?>
-				<tr>
-  				<td class="label">
-    				<label>Количество:</label>
-  				</td>
-  				<td>
-    				<?php   		
-      				woocommerce_quantity_input( array(
-          			'min_value'   => apply_filters( 'woocommerce_quantity_input_min', $product->get_min_purchase_quantity(), $product ),
-          			'max_value'   => apply_filters( 'woocommerce_quantity_input_max', $product->get_max_purchase_quantity(), $product ),
-          			'input_value' => isset( $_POST['quantity'] ) ? wc_stock_amount( $_POST['quantity'] ) : $product->get_min_purchase_quantity(),
-          		) ); 
-            ?>
-  				</td>
-				</tr>
-			</tbody>
-		</table>
 
-		<?php do_action( 'woocommerce_before_add_to_cart_button' ); ?>
+    <div class="product_meta variations">
+  		<?php if ( wc_product_sku_enabled() && ( $product->get_sku() || $product->is_type( 'variable' ) ) ) : ?>
+        <div>
+          <label class="color-tin"><?php esc_html_e( 'SKU:', 'woocommerce' ); ?></label>
+  	      <div class="sku_wrapper inline">
+  	        <span class="sku"><?php echo ( $sku = $product->get_sku() ) ? $sku : esc_html__( 'N/A', 'woocommerce' ); ?></span>
+          </div>
+        </div>
+  	  <?php endif; ?>
+  	  
 
-  	<?php
-  		/**
-  		 * @since 3.0.0.
-  		 */
-  		do_action( 'woocommerce_before_add_to_cart_quantity' );
-  
-  		/**
-  		 * @since 3.0.0.
-  		 */
-  		do_action( 'woocommerce_after_add_to_cart_quantity' );
-  		
-/*       echo end( $attribute_keys ) === $attribute_name ? apply_filters( 'woocommerce_reset_variations_link', '<a class="reset_variations" href="#">' . esc_html__( 'Clear', 'woocommerce' ) . '</a>' ) : ''; */
+  	  
+  	  <?php 
+    	  // we are trying to show all others attributes that is not in a variation
       
-  	?>
-
-		<div class="single_variation_wrap row">				
-				<div class="col col-xs-12 col-sm-6 row row-centered">
-  				<div class="woocommerce-variation single_variation"></div>
-				</div>
-				
-        <div class="col col-xs-12 col-sm-6 woocommerce-variation-add-to-cart variations_button text-center">
-      <?php
+      $allAttr = $product->get_attributes();
+      
+      foreach ( $allAttr as $attribute ) : 
+        $values = array();
+        if ( $attribute->is_taxonomy() ) {
+      		$attribute_taxonomy = $attribute->get_taxonomy_object();
+      		
+      		//name like a Woo categoty slug
+      		$tax = 'pa_' .$attribute_taxonomy->attribute_name;
+      
+      		
+      		$attribute_values = wc_get_product_terms( $product->get_id(), $attribute->get_name(), array( 'fields' => 'all' ) );
+          
+          //Check if this attribute is in the variation?
+          if( !array_key_exists($tax, $attributes) ){?>
+            <div class="">
+    					<label class="attribute-label inline"><?php echo $attribute_taxonomy->attribute_label ?>: </label>
+        			<?php foreach ( $attribute_values as $attribute_value ) {
+        				$value_name = esc_html( $attribute_value->name );
+        
+        				if ( $attribute_taxonomy->attribute_public ) {
+        					$values[] = '<a href="' . esc_url( get_term_link( $attribute_value->term_id, $attribute->get_name() ) ) . '" class="attribute-link" rel="tag">' . $value_name . '</a>';
+        				} else {
+        					$values[] = $value_name;
+        				}
+        				echo apply_filters( 'woocommerce_attribute', wptexturize( implode( ', ', $values ) ), $attribute, $values );
+        			}?> 
+            </div>
+          <?php
+          }
+      	};
+    
+      endforeach;
+      ?>
+  	  
+  	  
+  	  
+  	  
+  		<?php foreach ( $attributes as $attribute_name => $options ) : ?>
+  		
+  	    <label class="attribute-label" for="<?php echo esc_attr( sanitize_title( $attribute_name ) ); ?>">
+  	      <?php echo esc_html( wc_attribute_label( $attribute_name ) ); ?>
+  	    </label>
+  	    
+  	    <div class="attribute-value">
+  				<?php
+  					$selected = isset( $_REQUEST[ 'attribute_' . $attribute_name ] ) ? wc_clean( urldecode( wp_unslash( $_REQUEST[ 'attribute_' . $attribute_name ] ) ) ) : $product->get_variation_default_attribute( $attribute_name ); // WPCS: input var ok, CSRF ok, sanitization ok.
+  	
+  					wc_dropdown_variation_attribute_options( array(
+  						'options'   => $options,
+  						'attribute' => $attribute_name,
+  						'product'   => $product,
+  						'selected'  => $selected,
+  					) );
+  	
+  					echo end( $attribute_keys ) === $attribute_name ? wp_kses_post( apply_filters( 'woocommerce_reset_variations_link', '') ) : '';
+  				?>
+  	    </div>
+  		<?php endforeach; ?>			
+    </div>
+    
+    
+		<div class="single_variation_wrap">
+			<?php
 				/**
-				 * woocommerce_before_single_variation Hook.
+				 * Hook: woocommerce_before_single_variation.
 				 */
 				do_action( 'woocommerce_before_single_variation' );
-        /**
-				 * woocommerce_single_variation hook. Used to output the cart button and placeholder for variation data.
+
+				/**
+				 * Hook: woocommerce_single_variation. Used to output the cart button and placeholder for variation data.
+				 *
 				 * @since 2.4.0
 				 * @hooked woocommerce_single_variation - 10 Empty div for variation data.
 				 * @hooked woocommerce_single_variation_add_to_cart_button - 20 Qty and cart button.
@@ -115,17 +124,11 @@ do_action( 'woocommerce_before_add_to_cart_form' ); ?>
 				do_action( 'woocommerce_single_variation' );
 
 				/**
-				 * woocommerce_after_single_variation Hook.
+				 * Hook: woocommerce_after_single_variation.
 				 */
 				do_action( 'woocommerce_after_single_variation' );
-      ?>
-			</div>
-		</div> <!-- single_variation_wrap -->
-		
-    
-      <?php do_action( 'woocommerce_after_add_to_cart_button' ); ?>
-    
-    
+			?>
+		</div>
 	<?php endif; ?>
 
 	<?php do_action( 'woocommerce_after_variations_form' ); ?>
